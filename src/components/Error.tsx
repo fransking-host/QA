e9bc2fed0db53f75d6bf9dcfc5a121b2e7aeb0ce
fransking-host <61174727+fransking-host@users.noreply.github.com>
2023-03-30 22:47:41 +0800

    Update Error.tsx

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,12 +1,6 @@
 export default (props: { info: string }) => (
   <p class="text-pink-6 my-5">
-    🚨 {props.info}，请稍后再试，或者
-    <a
-      href="https://github.com/gcloudlab/QA/issues"
-      target="_blank"
-      class=" underline hover:text-black">
-      反馈问题
-    </a>
+    🚨 {props.info}，请稍后再试
     。
   </p>
 );
